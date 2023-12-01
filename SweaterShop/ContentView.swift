ef8cceb7114dbb3ed0bfa3540c3976206616034f
Sneha Tyagi <snehtyagi9810@gmail.com>
2023-12-01 15:48:12 +0530

    Initial Commit

---
+++ SweaterShop/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SweaterShop
+//
+//  Created by Sneh Tyagi on 01/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SweaterShop/SweaterShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweaterShopApp.swift
+//  SweaterShop
+//
+//  Created by Sneh Tyagi on 01/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SweaterShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
